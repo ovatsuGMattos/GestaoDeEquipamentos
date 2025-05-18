0c043a064b95531eb85ddde7302eff79413386dc
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-05-18 17:15:27 -0300

    atualizado pasta Util

--- GestaoDeEquipamentos.ConsoleApp/Util/Notificador.cs
+++ GestaoDeEquipamentos.ConsoleApp/Util/Notificador.cs
@@ -1,15 +1,17 @@
-﻿namespace GestaoDeEquipamentos.ConsoleApp.Util
-{
-    public static class Notificador
-    {
-        public static void ExibirMensagem(string mensagem, ConsoleColor cor)
-        {
-            Console.ForegroundColor = cor;
+﻿namespace GestaoDeEquipamentos.ConsoleApp.Util;
 
-            Console.WriteLine();
-            Console.WriteLine(mensagem);
-            Console.ResetColor();
-            Console.ReadLine();
-        }
+public static class Notificador
+{
+    public static void ExibirMensagem(string mensagem, ConsoleColor cor)
+    {
+        Console.ForegroundColor = cor;
+
+        Console.WriteLine();
+
+        Console.WriteLine(mensagem);
+
+        Console.ResetColor();
+
+        Console.ReadLine();
     }
 }
