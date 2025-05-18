83e79d858b0b7bd7750b772b3632d5ce376d7f94
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-05-18 17:13:48 -0300

    atualizado modulo fabricante

--- GestaoDeEquipamentos.ConsoleApp/ModuloFabricante/Fabricante.cs
+++ GestaoDeEquipamentos.ConsoleApp/ModuloFabricante/Fabricante.cs
@@ -74,9 +74,4 @@ public class Fabricante : EntidadeBase<Fabricante>
         Email = fabricanteEditado.Email;
         Telefone = fabricanteEditado.Telefone;
     }
-
-    public override string ToString()
-    {
-        return $"Id: {Id}, Nome: {Nome}, Email: {Email}, Telefone: {Telefone}";
-    }
 }
