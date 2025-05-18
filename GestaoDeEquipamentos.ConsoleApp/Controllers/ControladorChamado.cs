d7e9d3a2060069c3502a5ca775cb5248af3d6f6d
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-05-18 17:30:15 -0300

    implementado e corrigido erros no codigo.

--- GestaoDeEquipamentos.ConsoleApp/Controllers/ControladorChamado.cs
+++ GestaoDeEquipamentos.ConsoleApp/Controllers/ControladorChamado.cs
@@ -5,21 +5,12 @@ using GestaoDeEquipamentos.ConsoleApp.ModuloChamado;
 using GestaoDeEquipamentos.ConsoleApp.ModuloEquipamento;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GestaoDeEquipamentos.ConsoleApp.Controllers;
-
 [Route("chamados")]
 public class ControladorChamado : Controller
 {
     private ContextoDados contextoDados;
     private IRepositorioChamado repositorioChamado;
     private IRepositorioEquipamento repositorioEquipamento;
-
-    public ControladorChamado()
-    {
-        contextoDados = new ContextoDados(true);
-        repositorioChamado = new RepositorioChamadoEmArquivo(contextoDados);
-        repositorioEquipamento = new RepositorioEquipamentoEmArquivo(contextoDados);
-    }
 
     [HttpGet("cadastrar")]
     public IActionResult Cadastrar()
@@ -46,81 +37,5 @@ public class ControladorChamado : Controller
         );
 
         return View("Notificacao", notificacaoVM);
-    }
-
-    [HttpGet("editar/{id:int}")]
-    public IActionResult Editar([FromRoute] int id)
-    {
-        var chamadoSelecionado = repositorioChamado.SelecionarRegistroPorId(id);
-
-        var equipamentos = repositorioEquipamento.SelecionarRegistros();
-
-        var editarVM = new EditarChamadoViewModel(
-            id,
-            chamadoSelecionado.Titulo,
-            chamadoSelecionado.Descricao,
-            chamadoSelecionado.Equipamento.Id,
-            equipamentos
-        );
-
-        return View(editarVM);
-    }
-
-    [HttpPost("editar/{id:int}")]
-    public IActionResult Editar([FromRoute] int id, EditarChamadoViewModel editarVM)
-    {
-        var equipamentos = repositorioEquipamento.SelecionarRegistros();
-
-        var chamadoOriginal = repositorioChamado.SelecionarRegistroPorId(id);
-
-        var chamadoEditado = editarVM.ParaEntidade(equipamentos);
-
-        if (chamadoEditado.Equipamento != chamadoOriginal.Equipamento)
-        {
-            chamadoOriginal.Equipamento.RemoverChamado(chamadoOriginal);
-            chamadoOriginal.Equipamento = chamadoEditado.Equipamento;
-        }
-
-        repositorioChamado.EditarRegistro(id, chamadoEditado);
-
-        var notificacaoVM = new NotificacaoViewModel(
-            "Chamado Editado!",
-            $"O registro \"{chamadoEditado.Titulo}\" foi editado com sucesso!"
-        );
-
-        return View("Notificacao", notificacaoVM);
-    }
-
-    [HttpGet("excluir/{id:int}")]
-    public IActionResult Excluir([FromRoute] int id)
-    {
-        var chamadoSelecionado = repositorioChamado.SelecionarRegistroPorId(id);
-
-        var excluirVM = new ExcluirChamadoViewModel(id, chamadoSelecionado.Titulo);
-
-        return View(excluirVM);
-    }
-
-    [HttpPost("excluir/{id:int}")]
-    public IActionResult ExcluirConfirmado([FromRoute] int id)
-    {
-        repositorioChamado.ExcluirRegistro(id);
-
-        var notificacaoVM = new NotificacaoViewModel(
-            "Chamado Excluído!",
-            "O registro foi excluído com sucesso!"
-        );
-
-        return View("Notificacao", notificacaoVM);
-    }
-
-    [HttpGet("visualizar")]
-    public IActionResult Visualizar()
-    {
-        var chamados = repositorioChamado.SelecionarRegistros();
-
-        var visualizarVM = new VisualizarChamadosViewModel(chamados);
-
-        return View(visualizarVM);
     }
 }
